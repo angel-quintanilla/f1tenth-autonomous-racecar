0763649287b051bb4fb7fe48c9acfcd09532e6df
Ryan Parker <rparker52003@gmail.com>
2023-09-20 22:00:14 -0700

    Changes of reactive_node.py, bro isn't cooking yet

--- gap_follow/scripts/reactive_node.py
+++ gap_follow/scripts/reactive_node.py
@@ -16,7 +16,6 @@ class ReactiveFollowGap(Node):
         lidarscan_topic = '/scan'
         drive_topic = '/drive'
         
-        # TODO: Subscribe to LIDAR
         self.laser_sub = self.create_subscription (
             LaserScan,
             '/scan',
@@ -24,7 +23,6 @@ class ReactiveFollowGap(Node):
             10
         )
 
-        # TODO: Publish to drive
         self.drive_pub = self.create_publisher(
             AckermannDriveStamped,
             '/drive',
@@ -37,68 +35,137 @@ class ReactiveFollowGap(Node):
         self.min_index = 0
         self.max_index = 0
 
-    def preprocess_lidar(self, ranges):
+        self.car_radius = 0.35/2
+
+    def preprocess_lidar(self, ranges, data):
         """ Preprocess the LiDAR scan array. Expert implementation includes:
             1.Setting each value to the mean over some window
             2.Rejecting high values (eg. > 3m)
         """
-        for i, val in enumerate(ranges): 
+        for i, val in enumerate(ranges):
             mean_of_values = 0
-            check_end = 0
-
-            for j in range(5):
+            check_end = False
+            # currentAngle = i*data.angle_increment + data.angle_min
+            # if (currentAngle < -1*(np.pi/2)):
+            #     ranges[i] = 9999
+            # elif (currentAngle > -1*(np.pi/2)):
+            #     ranges[i] = 9999
+            # else:
+                # for the next 5 indices
+            for j in range(self.window):
+                # if out of bounds, break
                 if (i+j >= len(ranges)):
-                    check_end = 1
+                    check_end = True
                     break
 
+                # if high value
                 if (ranges[j] > self.depth):
+                    # set high value to max allowed
                     ranges[j] = self.depth
 
+                # add value to mean
                 mean_of_values += ranges[i+j]
 
-            if (check_end == 0):
+            # if not end of loop
+            if (check_end == False):
+                # change value at i to mean over window
                 ranges[i] = mean_of_values/self.window
 
         proc_ranges = ranges
         return proc_ranges
 
     def find_max_gap(self, free_space_ranges):
-        """ Return the start index & end index of the max gap in free_space_ranges
-        """
-        return None
+        """Return the start index & end index of the max gap in free_space_ranges"""
+        start_i = self.min_index
+        start_j = self.min_index
+        in_range = False
+        temp_i = 0
+        temp_j = 0
+
+        # for the range of 90 to -90
+        for n in range(self.min_index, self.max_index):
+            # if val 0, and temp range > start range (bigger gap)
+            if(free_space_ranges[n] == 0 and temp_j-temp_i > start_j-start_i):
+                # update start indices with bigger gap
+                start_i = temp_i
+                start_j = temp_j
+                in_range = False # reset in_range
+            elif not(in_range):
+                temp_i = n # update temp index i
+                in_range = True # true bool
+            else:
+                temp_j = n # update temp index j
+
+        # return start and end indices of max gap
+        return start_i, start_j
     
     def find_best_point(self, start_i, end_i, ranges):
-        """Start_i & end_i are start and end indicies of max-gap range, respectively
+        """ Start_i & end_i are start and end indicies of max-gap range, respectively
         Return index of best point in ranges
-	    Naive: Choose the furthest point within ranges and go there
-        """
-        return None
+	    Naive: Choose the furthest point within ranges and return index"""
+        furthest_point = start_i
+        for x in range(start_i, end_i+1):
+            if ranges[x] > ranges[furthest_point]:
+                furthest_point = x
+
+        return furthest_point
 
     def lidar_callback(self, data):
         """ Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
         """
         ranges = data.ranges
-        proc_ranges = self.preprocess_lidar(ranges)
+        proc_ranges = self.preprocess_lidar(ranges, data)
 
-        # only do this if statement once
+        # only do this if statement once, *initialize*
         if not(self.min_index and self.max_index):
-            # initialize angle at horizontal right
+            # initialize angle at perpendicular right
             self.min_index = int((45*(np.pi/180)) / data.angle_increment) 
-            # initialize angle at horizontal left
+            # initialize angle at perpendicular left
             self.max_index = int(self.min_index + (np.pi / data.angle_increment))
 
-        # for x in range(min_index, max_index)
+        # for x in range(self.min_index, self.max_index)
 
-        # TODO:
+        closest_index = 0
         #Find closest point to LiDAR
+            # Loop through entire valid range of indices
+        for x in range(self.min_index, self.max_index):
+            # if the value at new index is less than closest index value
+            if proc_ranges[x] < proc_ranges[closest_index]:
+                closest_index = x # set closest_index to new index
 
-        #Eliminate all points inside 'bubble' (set them to zero) 
+
+        distance_single_radian = np.tan(1) * proc_ranges[closest_index] # this gets x
+        distance_needed_in_radians = self.car_radius/distance_single_radian
+        number_of_indices = int(distance_needed_in_radians/data.angle_increment)
+
+        #Eliminate all points inside 'bubble' (set them to zero)
+        for n in range(closest_index-number_of_indices, closest_index+number_of_indices+1):
+            proc_ranges[n] = 0
+
 
         #Find max length gap 
+        start_i, start_j = self.find_max_gap(proc_ranges)
+        # print(start_i)
+        # print((start_i * data.angle_increment + data.angle_min)*180/np.pi)
+        # print(start_j)
+        # print((start_j * data.angle_increment + data.angle_min)*180/np.pi)
 
-        #Find the best point in the gap 
+
+        #Find the best point in the gap
+        best_point = self.find_best_point(start_i, start_j, proc_ranges)
+
+        # print(best_point)
+        # print((best_point * data.angle_increment + data.angle_min)*180/np.pi)
+        # print()
+        # print(proc_ranges[best_point])
+
+        # exit()
 
         #Publish Drive message
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.speed = 0.25
+        drive_msg.drive.steering_angle = best_point*data.angle_increment + data.angle_min
+        self.drive_pub.publish(drive_msg)
 
 
 def main(args=None):
