d9a1e70acc384d62ce73f7fe74c4d23eba0c3050
Ryan Parker <rparker52003@gmail.com>
2023-09-25 17:06:39 -0700

    Added speed, implemented corner clamp. TODO: Disparity Extender Approach

--- gap_follow/scripts/reactive_node.py
+++ gap_follow/scripts/reactive_node.py
@@ -28,26 +28,17 @@ class ReactiveFollowGap(Node):
             10
         )
 
-        self.window = 5
-        self.depth  = 3.0
-        self.car_radius = 0.5/2
-
-        self.laser_sub = self.create_subscription (
-            LaserScan,
-            lidarscan_topic,
-            self.lidar_callback,
-            10
-        )
-
-        self.drive_pub = self.create_publisher(
-            AckermannDriveStamped,
-            drive_topic,
-            10
-        )
-        self.MAX_ADMISSABLE_DIST = 30.0 # floor all greater values to this if scanned
-        self.MIN_ADMISSABLE_DIST = 0.3 # anything 1.5 meters away from us is now not even considered for gaps
-        self.CAR_RADIUS = 1.2
         self.VELOCITY = 1.0
+        self.MAX_ADMISSABLE_DIST = 3.0 # floor all greater values to this if scanned
+        self.MIN_ADMISSABLE_DIST = 0.3 # anything 0.3 meters away from us is now not even considered for gaps
+        self.CAR_RADIUS = .5
+        self.SAFE_WALL_DISTANCE = 0.1
+
+        self.STRAIGHT_AHEAD_SPEED = 3.0
+        self.STRAIGHT_AHEAD_THRESHOLD = 10*np.pi/180
+        self.WIDE_TURN_SPEED = 2.0
+        self.WIDE_TURN_THRESHOLD = 20*np.pi/180
+        self.SHARP_TURN_SPEED = 0.75
 
 
     def preprocess_lidar(self, ranges):
@@ -102,10 +93,6 @@ class ReactiveFollowGap(Node):
         # print(max_gap_i)
         # print(max_gap_j)
         return max_gap_i, max_gap_j
-    
-
-
-
 
     def find_best_point(self, start_i, end_i, ranges):
         """ Start_i & end_i are start and end indicies of max-gap range, respectively
@@ -113,24 +100,21 @@ class ReactiveFollowGap(Node):
 
 	    Naive: Choose the furthest point within ranges and go there
         """
-        max_index = start_i 
+        max_index_start = start_i 
+        max_index_end = start_i
         for i in range(start_i, end_i+1):
-            if ranges[i] > ranges[max_index]:
-                max_index = i
-        return max_index
+            if ranges[i] >= ranges[max_index_start]:
+                max_index_start = i
+            if ranges[i] > ranges[max_index_end]:
+                max_index_end = i
+        return int((max_index_start+max_index_end)/2)
+        # return (int(end_i+start_i/2))
 
-
-
-
-
-    def bubble(self, ranges, direct_right_index, direct_left_index, angle_increment, angle_min):
+    def bubble(self, ranges, direct_right_index, direct_left_index, angle_increment):
         """
         Find closest point to LiDAR
         Eliminate all points inside 'bubble' (set them to zero) 
         """
-
-
-
         #get out min index 
         min_index = direct_right_index
         for i in range(direct_right_index, direct_left_index+1):
@@ -153,7 +137,7 @@ class ReactiveFollowGap(Node):
             if currentBubbleLeft > 0:
                 ranges[currentBubbleLeft] = 0
         
-        return
+        return min_index
 
         
     def print_ranges_fancy(self, data):
@@ -185,23 +169,54 @@ class ReactiveFollowGap(Node):
         direct_left_index = int(direct_right_index+np.pi/data.angle_increment)
         
         
-        self.bubble(ranges, direct_right_index, direct_left_index, data.angle_increment, angle_min=data.angle_min)
+        self.bubble(ranges, direct_right_index, direct_left_index, data.angle_increment)
         
 
         #Find max length gap 
         maxGapStart_i, maxGapEnd_i = self.find_max_gap(ranges, direct_right_index, direct_left_index)
         # print(f"From {maxGapStart_i} to {maxGapEnd_i}")
         # self.print_ranges_fancy(data=data)
-        
+
         #Find the best point in the gap 
         best_point = self.find_best_point(ranges=ranges,start_i=maxGapStart_i, end_i=maxGapEnd_i)
-        print((best_point*data.angle_increment+data.angle_min)*180/np.pi)
+        # print(best_point)
         # self.print_ranges_fancy(data=data)
+
+        # If any point in range -135 to -90 or 90 to 135 is close, go straight
+        for x in range(0, direct_right_index+1):
+            if (0 < best_point < int(len(data.ranges)/2)):
+                if data.ranges[x] < self.SAFE_WALL_DISTANCE:
+                    best_point = int(len(data.ranges)/2)
+        for x in range(direct_left_index, len(data.ranges)):
+            if (int(len(data.ranges)/2) < best_point < len(data.ranges)):
+                if data.ranges[x] < self.SAFE_WALL_DISTANCE:
+                    best_point = int(len(data.ranges)/2)
+
+
+        angle = best_point*data.angle_increment + data.angle_min
+
+        # # clamp based on anything greater than a wider turn
+        # if(angle < self.WIDE_TURN_THRESHOLD*-1):
+        #     angle = self.WIDE_TURN_THRESHOLD*-1
+        # elif angle > self.WIDE_TURN_THRESHOLD:
+        #     angle = self.WIDE_TURN_THRESHOLD
+        
+        # give speeds dependant on our angle 
+        if ranges[best_point] < self.MAX_ADMISSABLE_DIST:
+            self.VELOCITY = self.SHARP_TURN_SPEED
+        elif(np.abs(angle) < self.STRAIGHT_AHEAD_THRESHOLD):
+            self.VELOCITY = self.STRAIGHT_AHEAD_SPEED
+        elif np.abs(angle) < self.WIDE_TURN_THRESHOLD:
+            self.VELOCITY = self.WIDE_TURN_SPEED
+        else:
+            self.VELOCITY = self.SHARP_TURN_SPEED
+
+        # exit()
 
         #Publish Drive message
         drive_msg = AckermannDriveStamped()
         drive_msg.drive.speed = self.VELOCITY
-        drive_msg.drive.steering_angle = best_point*data.angle_increment + data.angle_min
+        drive_msg.drive.steering_angle = angle
         self.drive_pub.publish(drive_msg)
 
 
